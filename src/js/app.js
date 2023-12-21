d2978b556a392d9d9183979248263d68c599a495
litvinovanton7 <antonafonin154@gmail.com>
2023-12-21 17:11:48 -0500

    feat: js grade btns

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const btns = document.querySelectorAll(".btn");
+
+    btns.forEach((btn) => {
+        btn.addEventListener('click', e => {
+            const target = e.target;
+
+            if (target && !target.classList.contains('grey')) {
+                btn.classList.add('grey');
+            } else {
+                btn.classList.remove('grey');
+            }
+        });
+    });
+});
