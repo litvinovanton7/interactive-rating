023b0401c1933fb9a7132fa70e27a3c0cc1a2295
litvinovanton7 <antonafonin154@gmail.com>
2023-12-21 17:21:44 -0500

    feat: functional for thank you wrapper

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-    const btns = document.querySelectorAll(".btn");
+    const btns = document.querySelectorAll('.btn');
+    const btnSubmit = document.querySelector('.btn__submit');
+    const wrapperGrade = document.querySelector('.wrapper__grade');
+    const wrapperThanks = document.querySelector('.wrapper__thank');
 
     btns.forEach((btn) => {
         btn.addEventListener('click', e => {
@@ -11,6 +14,17 @@ window.addEventListener("DOMContentLoaded", () => {
                 btn.classList.add('grey');
             } else {
                 btn.classList.remove('grey');
+            }
+        });
+    });
+
+    btnSubmit.addEventListener('click', e => {
+        const target = e.target;
+
+        btns.forEach(btn => {
+            if (btn.classList.contains('grey')) {
+                wrapperThanks.style.display = 'block';
+                wrapperGrade.style.display = 'none';
             }
         });
     });
